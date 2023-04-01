8ac207a82de2e3e5adbe84983c8d09e81ba9ce5f
Fabyo Silveira <fabyo.silveira@iebtinnovation.com>
2023-04-01 14:39:24 -0300

    - Login page working for mock user - Fix bug that next was not loading styles - Applying error on login and redirecting to first page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,77 @@
-import Head from 'next/head'
 import { NextPage } from 'next'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
-export const Home: NextPage = () => {
+import {
+  ErrorSpanStyle,
+  LoginBoxDiv,
+  StyledButton,
+  StyledInput,
+  WrapperDiv,
+} from './pageStyles'
+import { Input } from '@/components/Input'
+
+const mockUser = {
+  user: 'fabyosilveira',
+  password: '123456',
+}
+
+export const Login: NextPage = () => {
+  const [error, setError] = useState(false)
+  const [login, setLogin] = useState({ user: '', password: '' })
+
+  const router = useRouter()
+
+  const onSubmit = () => {
+    if (login.user !== mockUser.user || login.password !== mockUser.password) {
+      setError(true)
+      return
+    }
+
+    router.push('/mural')
+  }
+
   return (
     <>
-      <Head>
-        <title>GradeRank</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <h1>Hello PDS App!</h1>
+      <WrapperDiv>
+        <img src={'/assets/logoPNG.png'} width='425px' height='150px' />
+        <LoginBoxDiv>
+          <h2>Faça seu login para começar a avaliar</h2>
+          <StyledInput
+            id='outlined-basic'
+            label='Login'
+            variant='outlined'
+            error={error}
+            value={login.user}
+            onChange={(e: any) => {
+              setError(false)
+              setLogin((prev) => ({ ...prev, user: e.target.value }))
+            }}
+          />
+          <Input
+            id='outlined-basic'
+            label='Senha'
+            variant='outlined'
+            type='password'
+            error={error}
+            value={login.password}
+            onChange={(e) => {
+              setError(false)
+              setLogin((prev) => ({ ...prev, password: e.target.value }))
+            }}
+          />
+          {error ? (
+            <ErrorSpanStyle>Usuário ou senha incorretos!</ErrorSpanStyle>
+          ) : (
+            <></>
+          )}
+          <StyledButton variant='contained' onClick={onSubmit}>
+            ENTRAR
+          </StyledButton>
+        </LoginBoxDiv>
+      </WrapperDiv>
     </>
   )
 }
 
-export default Home
+export default Login
---
+++ src/pages/mural.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+
+export const Mural: NextPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1>Welcome to Grade Rank!</h1>
+    </div>
+  )
+}
+
+export default Mural
