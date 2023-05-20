4ca6d3919ab710970f756ed18928b4742ca89a4e
umaanaflavia <anaflavia20011@gmail.com>
2023-05-20 00:30:18 -0300

    Criando página "Meu perfil"

--- src/components/AuthPageLayout/index.tsx
+++ src/components/AuthPageLayout/index.tsx
@@ -6,7 +6,7 @@ export const AuthPageLayout = ({ children }: AuthPageLayoutProps) => {
     <div style={{ width: '100vw', height: '100vh' }}>
       <BackgroundAuthLayout>
         <AuthWrapperBoxDiv>
-          <img src={'/assets/logoPNG.png'} width='425px' height='150px' />
+          <img src={'/assets/logoPNG.png'} width='425px' height='145px' />
           <LoginBoxDiv>{children}</LoginBoxDiv>
         </AuthWrapperBoxDiv>
       </BackgroundAuthLayout>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,40 +1,125 @@
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-import { Loading } from '@/components/Loading'
-import { PageLayout } from '@/components/PageLayout'
-import { getUserCookie } from '@/utils/auth'
-import { LoggedUser } from '@/utils/auth/type'
+import { Loading } from '@/components/Loading';
+import { PageLayout } from '@/components/PageLayout';
+import { getUserCookie } from '@/utils/auth';
+import { LoggedUser } from '@/utils/auth/type';
+
+import { 
+  Row,
+  HorizontalRule,
+  FormContainer,
+  FormGroup,
+  Label,
+  Input,
+  SubmitButton,
+  ReverseButton
+} from './pageStyles/Home';
+
+interface FormData {
+  nome: string;
+  email: string;
+  matricula: string;
+  senha: string;
+}
 
 export const Home: NextPage = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    const loggedUser = getUserCookie()
+    const loggedUser = getUserCookie();
 
     if (!loggedUser) {
-      router.push('/')
+      router.push('/');
     } else {
-      setUser(loggedUser as LoggedUser)
-      setLoading(false)
+      setUser(loggedUser as LoggedUser);
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState<LoggedUser>()
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<LoggedUser>();
+  const [formData, setFormData] = useState<FormData>({
+    nome: '',
+    email: '',
+    matricula: '',
+    senha: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData); // You can perform any action with the form data here
+  };
 
   return (
     <>
       {!loading ? (
         <PageLayout user={user?.name} activeMenuItem={0}>
-          <h1>{`Bem vindo ao seu perfil, ${user?.name}!`}</h1>
+          <Row>
+            <h1>{`Bem vindo ao seu perfil, ${user?.name}!`}</h1>
+            <HorizontalRule />
+          </Row>
+          <FormContainer onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label htmlFor="nome">Nome</Label>
+              <Input
+                type="text"
+                id="nome"
+                name="nome"
+                value={formData.nome}
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="matricula">Matrícula</Label>
+              <Input
+                type="text"
+                id="matricula"
+                name="matricula"
+                value={formData.matricula}
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="senha">Senha</Label>
+              <Input
+                type="password"
+                id="senha"
+                name="senha"
+                value={formData.senha}
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <SubmitButton type="submit">SALVAR</SubmitButton>
+            </FormGroup>
+            <FormGroup>
+              <ReverseButton type="reverse">REVERTER ALTERAÇÕES</ReverseButton>
+            </FormGroup>
+          </FormContainer>
         </PageLayout>
       ) : (
         <Loading />
       )}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
